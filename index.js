888f7a75b3b2e5d48a8c87b2480d6055f970601e
unknown <ivankozachuk10@gmail.com>
2021-03-28 12:42:38 +0200

    ready for heroku deployment

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import 'path';
+
+
+import dcrRoutes from './routes/dcr.js';
+import userRoutes from './routes/users.js';
+
+const app = express();
+
+app.use(bodyParser.json({limmit: '5mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
+app.use(cors());
+
+app.use('/dcr', dcrRoutes);
+app.use('/user', userRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://ivan_kozachuk:ivan_kozachuk123@dcr.hwnfk.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+// Serve static assets if in prod
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
